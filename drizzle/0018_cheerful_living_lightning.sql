40d652577ecec760e3048df4d9b67eb898a8084f
Petar Atanasovski <patanasovski25@gmail.com>
2026-03-30 19:26:52 +0200

    Implement isAlumni flag to users table and add filters

---
+++ drizzle/0018_cheerful_living_lightning.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "users" ADD COLUMN "is_alumni" boolean DEFAULT false NOT NULL;
--- src/constants/types.ts
+++ src/constants/types.ts
@@ -14,6 +14,7 @@ export interface AdminUser {
   yearOfStudies: number | null;
   profileCompleted: boolean;
   role: Role;
+  isAlumni: boolean;
   activeUntil: string | null;
   isActive: boolean;
   imageUrl: string | null;
